9145c40aeedb7c1e822a02d670bcee15ad5a8edf
Derek <dphtran501@yahoo.com>
2017-11-02 15:14:49 -0700

    finished activity_main.xml

--- app/src/main/java/edu/orangecoastcollege/cs273/touchgestures/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/touchgestures/MainActivity.java
@@ -5,7 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     private ScrollView gesturesScrollView;
 
@@ -19,10 +20,18 @@ public class MainActivity extends AppCompatActivity {
     private int singleTaps = 0, doubleTaps = 0, longPresses = 0, scrolls = 0, flings = 0;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        gesturesScrollView = (ScrollView) findViewById(R.id.gesturesScrollView);
+        gesturesLogTextView = (TextView) findViewById(R.id.gesturesLogTextView);
+        singleTapTextView = (TextView) findViewById(R.id.singleTapTextView);
+        doubleTapTextView = (TextView) findViewById(R.id.doubleTapTextView);
+        longPressTextView = (TextView) findViewById(R.id.longPressTextView);
+        scrollTextView = (TextView) findViewById(R.id.scrollTextView);
+        flingTextView = (TextView) findViewById(R.id.flingTextView);
 
     }
 
